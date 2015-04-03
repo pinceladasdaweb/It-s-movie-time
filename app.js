06a4ceb6fefc7455bd4e53f32acce76b26a72b45
Pedro Rogério <pinceladasdaweb@hotmail.com>
2015-04-02 22:51:26 -0300

    Call new route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express   = require('express'),
     path      = require('path'),
     index     = require('./routes/index'),
     movie     = require('./routes/movie'),
+    page      = require('./routes/page'),
     app       = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -10,6 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public', {maxAge: 86400000}));
 
 app.use('/', index);
+app.use('/page', page);
 app.use('/movie', movie);
 
 module.exports = app;
